dcc5ef18ea177e012c8a0afb3dc66058ae4d3e01
LU Jiaying <lujiaying7@gmail.com>
2019-07-15 22:42:25 +0200

    ruby testing stuff

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -1,5 +1,38 @@
-
- class "Simon says" 
   def echo (something)
-    return "something"    
+    return something   
   end 
+
+  def shout (s)
+    return s.upcase
+  end
+
+  def repeat(t, n=2)
+    return n.times.collect{t}.join(' ')
+  end  
+
+  def start_of_word(s, n)
+    return s[0...n]
+  end  
+  
+  def first_word(s)
+     return s.split[0]
+  end
+  
+  def titleize(s)
+    words = s.split
+      words.each do |word|
+      if word.length < 3
+         word.downcase!
+
+      elsif word.length > 3 ||
+         words.index(word) == 0
+         word.capitalize!
+      end
+    end 
+      return words.join(' ')   
+  end   
+
+
+  
+
+ 
